fd319b6c8909e4d79fb9fb9c74b32951c4508382
David Amedi <amukoahdavid@gmail.com>
2025-04-21 23:42:50 +0300

    refactor: enhance plant management features with form handling and search functionality

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,18 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({ plants, setPlants }) {
+  // console.log("Plant IDs:", plants.map((plant) => plant.id));
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">
+      {plants.map((plant, index) => (
+        <PlantCard
+          key={plant.id ?? `${plant.name}-${index}`} // Fallback to name-index if id is undefined
+          plant={plant}
+          setPlants={setPlants}
+        />
+      ))}
+    </ul>
   );
 }
 
